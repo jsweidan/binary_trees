dad5b2600a369ca3fadb22f4bdf69415101f3b43
jsweidan <jawaheryslam@gmail.com>
2024-02-28 14:19:44 -0800

    a function that deletes an entire binary tree

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -1,14 +1,14 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_insert_left - inserts a node as the left-child of another node
- * @parent: a pointer to the node that inserts the left child in
+ * binary_tree_insert_right - inserts a node as the right-child of another node
+ * @parent: a pointer to the node that inserts the right child in
  * @value: value to store in the new node
  * Return: return a pointer to the created node,
  *         or NULL on failure or if parent is NULL
  */
 
-binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
+binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 {
 	binary_tree_t *new;
 
@@ -19,12 +19,12 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 	if (new == NULL)
 		return (NULL);
 
-	if (parent->left != NULL)
+	if (parent->right != NULL)
 	{
-		new->left = parent->left;
-		parent->left->parent = new;
+		new->right = parent->right;
+		parent->right->parent = new;
 	}
-	parent->left = new;
+	parent->right = new;
 
 	return (new);
 }
